a66957cafdcc5797b58a13164a59afdedfd9d410
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-11 19:56:25 -0500

    added insert function to linked list

--- linked-list.js
+++ linked-list.js
@@ -31,11 +31,46 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    traverseToIndex(index) {
+        let currentNode = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            if (!currentNode.next) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+        } else {
+            const currentNode = this.traverseToIndex(index);
+            const newNode = new Node(value);
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+            return this.printList();
+        }
+
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(13);
-
-console.log(myLinkedList);
+myLinkedList.insert(44, 99);
+console.log(myLinkedList.printList());
